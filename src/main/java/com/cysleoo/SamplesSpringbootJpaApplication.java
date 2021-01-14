20d06c076f4f6ce49d0c40935bc0b84f42180f75
ssliu <ssliu@chindigg.com>
2021-01-14 15:51:24 +0800

    init

---
+++ src/main/java/com/cysleoo/SamplesSpringbootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.cysleoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplesSpringbootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplesSpringbootJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cysleoo/SamplesSpringbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cysleoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplesSpringbootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
